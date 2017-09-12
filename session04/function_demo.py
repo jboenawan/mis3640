2c27cb9c44f90f47e2a2b906001e19f183a5a40d
Zhi Li <zli@babson.edu>
2017-09-12 15:02:15 -0400

    finished session 4

---
+++ session04/function_demo.py
@@ -0,0 +1,24 @@
+def print_lyrics():
+    print("Hey Jude. Don't make it bad.")
+    print("Take a sad song and make it better.")
+
+# print_lyrics()
+
+print(type(print_lyrics))
+
+def repeat_lyrics():
+    print_lyrics()
+    print('Na - na - na - na - na, na - na - na - na')
+    print_lyrics()
+
+# repeat_lyrics()
+
+def print_twice(name):
+    print(name)
+    print(name)
+
+print_twice('Andrew')
+
+my_name = 'Zhi'
+print_twice(my_name)
+
