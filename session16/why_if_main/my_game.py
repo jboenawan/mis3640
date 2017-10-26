2dc38c38d190babe9a14ad677399b019c1e3dc55
Zhi Li <zli@babson.edu>
2017-10-26 12:20:40 -0400

    added oop

---
+++ session16/why_if_main/my_game.py
@@ -0,0 +1,6 @@
+import my_math
+
+print("this is my_game.")
+
+n = int(input())
+print(my_math.square(n))
---
+++ session16/why_if_main/my_math.py
@@ -0,0 +1,10 @@
+def square(x):
+    return x * x
+
+
+def main():
+    year = int(input())
+    print("In my_math: square(2016) ==", square(year))
+
+if __name__ == '__main__':
+    main()
