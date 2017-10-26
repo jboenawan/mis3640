8d35b82ac9394ce953a8f9aa021fe6a246ad2537
Zhi Li <zli@babson.edu>
2017-10-26 14:57:21 -0400

    updated Point1.py after session 16

--- OOP/OOP1/Point1.py
+++ OOP/OOP1/Point1.py
@@ -1,3 +1,5 @@
+import math
+
 class Point:
     """Represents a point in 2-D space.
 
@@ -8,15 +10,27 @@ class Point:
 # new_point = Point()
 # new_point.x = 100
 # new_point.y = 50
-# print(new_point)
+# # print(new_point)
+# print(new_point.x, new_point.y)
 
+# Julia_point=Point()
+# Julia_point.x = 50
+# Julia_point.y = 60
+# print(Julia_point.x, Julia_point.y)
+
+# x = Julia_point.y
+# print(x)
+# print(Julia_point.x, Julia_point.y)
 
 def print_point(p):
     """Print a Point object in human-readable format."""
-    print('(%g, %g)' % (p.x, p.y))
+    print('({}, {}).'.format(p.x, p.y))
 
+# new_point = Point()
+# new_point.x = 100
+# new_point.y = 50
 
-
+# print_point(new_point)
 
 def distance_between_points(p1, p2):
     """Computes the distance between two Point objects.
@@ -26,7 +40,14 @@ def distance_between_points(p1, p2):
 
     returns: float
     """
+    distance = math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
+    return distance
 
+# Alex_point = Point()
+# Alex_point.x = 40
+# Alex_point.y = -30
+
+# print(distance_between_points(new_point, Alex_point))
 
 
 class Rectangle:
@@ -39,8 +60,8 @@ class Rectangle:
 # Alex_rect.width = 10
 # Alex_rect.height = 20
 # Alex_rect.corner = Point()
-# Alex_rect.corner.x = 1
-# Alex_rect.corner.y = 1
+# Alex_rect.corner.x = 0
+# Alex_rect.corner.y = 0
 
 # print_point(Alex_rect.corner)
 # print_point(my_point)
@@ -90,43 +111,43 @@ def print_rectangle(rect):
 
 
 def main():
-    # my_point = Point()
-    # print(Point.__doc__)
-    # my_point.x = 3
-    # my_point.y = 4
-    # print('My point', end=' ')
-    # print_point(my_point)
+    my_point = Point()
+    print(Point.__doc__)
+    my_point.x = 3
+    my_point.y = 4
+    print('My point', end=' ')
+    print_point(my_point)
 
-    # print('Is my_point an instance of Point?', isinstance(my_point, Point))
-    # print('Is my_point an instance of Rectangle?',
-    #       isinstance(my_point, Rectangle))
-    # print('Does my_point have an attribute x?', hasattr(my_point, 'x'))
-    # print('Does my_point have an attribute z?', hasattr(my_point, 'z'))
+    print('Is my_point an instance of Point?', isinstance(my_point, Point))
+    print('Is my_point an instance of Rectangle?',
+          isinstance(my_point, Rectangle))
+    print('Does my_point have an attribute x?', hasattr(my_point, 'x'))
+    print('Does my_point have an attribute z?', hasattr(my_point, 'z'))
 
-    # box = Rectangle()
-    # box.width = 100.0
-    # box.height = 200.0
-    # box.corner = Point()
-    # box.corner.x = 0.0
-    # box.corner.y = 0.0
+    box = Rectangle()
+    box.width = 100.0
+    box.height = 200.0
+    box.corner = Point()
+    box.corner.x = 0.0
+    box.corner.y = 0.0
 
-    # print('Does box have an attribute x?', hasattr(box, 'x'))
+    print('Does box have an attribute x?', hasattr(box, 'x'))
 
-    # print('Does box have an attribute corner?', hasattr(box, 'corner'))
+    print('Does box have an attribute corner?', hasattr(box, 'corner'))
 
-    # print('Rectangle has these:', dir(box))
+    print('Rectangle has these:', dir(box))
 
-    # center = find_center(box)
-    # print('center', end=' ')
-    # print_point(center)
+    center = find_center(box)
+    print('center', end=' ')
+    print_point(center)
 
-    # print(box.width)
-    # print(box.height)
-    # print('grow')
-    # grow_rectangle(box, 50, 100)
-    # print(box.width)
-    # print(box.height)
-    pass
+    print(box.width)
+    print(box.height)
+    print('grow')
+    grow_rectangle(box, 50, 100)
+    print(box.width)
+    print(box.height)
+
 
 if __name__ == '__main__':
     main()
